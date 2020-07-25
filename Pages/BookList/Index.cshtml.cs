fa951929e8ef95a09ceab7452b249908bfd4e720
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-25 16:56:13 +0100

    adding an Index page for BookList

---
+++ Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList_Razor_Pages.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookList_Razor_Pages.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
+        {
+           Books = await _db.Books.ToListAsync();
+        }
+    }
+}
