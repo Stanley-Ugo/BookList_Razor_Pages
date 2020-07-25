0eb43c417fe5676add391f03153f9073599e9623
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-25 15:34:41 +0100

    adding a Book Model

---
+++ Model/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList_Razor_Pages.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public int Availability { get; set; }
+        public double Price { get; set; }
+    }
+}
